cc09aa4e8fad0593d3aff7d6ea8782dd8470e6ed
Shifeng Huang <shifeng.huang@hpe.com>
2016-11-01 07:21:52 +0800

    tag

--- tests/beautifulsoup/about_BeautifulSoup.py
+++ tests/beautifulsoup/about_BeautifulSoup.py
@@ -30,13 +30,18 @@ class BeautifulSoupTest(unittest.TestCase):
         self.assertEqual(soup.title.get_text(), "The Dormouse's story")
 
     def test_tag(self):
-        soup = BeautifulSoup('<b>b1</b><b>b1</b>', "html5lib")
+        soup = BeautifulSoup('<b>b2</b><b>b1</b>', "html5lib")
         tag = soup.b
         self.assertEqual(type(tag), bs4.element.Tag)
 
         # 每使用一次.<tag-name>,就会一个tag，下次在调用的时候，会获取下一个tag
-        self.assertEqual(str(tag), '<b>b1</b>')
-        self.assertEqual(str(soup.b), '<b>b2</b>')
+        # ???? 这个为什么昨天运行不是这样的结果！！！！
+        # 看起来是取得第一个满足条件的tag
+        self.assertEqual('<b>b2</b>', str(tag))
+        self.assertEqual('<b>b2</b>', str(soup.b))
+        # self.assertEqual('<b>b2</b>', str(soup.b))
+
+        # 整个BeautifulSoup对象可以当做一个Tag来使用
 
     def test_tag_name(self):
         soup = BeautifulSoup('<b class="boldest">Extremely bold</b>', "html5lib")
@@ -59,3 +64,18 @@ class BeautifulSoupTest(unittest.TestCase):
 
         # 同样可以通过赋值的方式来修改属性
         pass
+
+    def test_tag_string(self):
+        soup = BeautifulSoup('<b>b2</b>', "html5lib")
+        tag = soup.b
+        self.assertEqual(type(tag.string), bs4.element.NavigableString)
+        # 可以通过unicode方法直接将tag.string转换成unicode string
+        # python3中unicode() 在哪里？
+        # unicode(tag.string)
+
+        # 同样可以通过赋值的方式来修改属性
+
+        # 如果想在Beautiful Soup之外使用 NavigableString 对象,
+        # 需要调用 unicode() 方法,将该对象转换成普通的Unicode字符串,
+        # 否则就算Beautiful Soup已方法已经执行结束,
+        # 该对象的输出也会带有对象的引用地址.这样会浪费内存.
