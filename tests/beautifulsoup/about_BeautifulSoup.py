60e0c4a76a987db7b45236392f22bb342646b428
Shifeng Huang <shifeng.huang@hpe.com>
2016-11-01 01:18:03 +0800

    ..

--- tests/beautifulsoup/about_BeautifulSoup.py
+++ tests/beautifulsoup/about_BeautifulSoup.py
@@ -1,4 +1,5 @@
 import unittest
+import bs4
 from bs4 import BeautifulSoup
 
 html_doc = """
@@ -27,3 +28,34 @@ class BeautifulSoupTest(unittest.TestCase):
 
         self.assertEqual(soup.title.name, "title")
         self.assertEqual(soup.title.get_text(), "The Dormouse's story")
+
+    def test_tag(self):
+        soup = BeautifulSoup('<b>b1</b><b>b1</b>', "html5lib")
+        tag = soup.b
+        self.assertEqual(type(tag), bs4.element.Tag)
+
+        # 每使用一次.<tag-name>,就会一个tag，下次在调用的时候，会获取下一个tag
+        self.assertEqual(str(tag), '<b>b1</b>')
+        self.assertEqual(str(soup.b), '<b>b2</b>')
+
+    def test_tag_name(self):
+        soup = BeautifulSoup('<b class="boldest">Extremely bold</b>', "html5lib")
+        tag = soup.b
+        # tag的名字可以通过name属性获得
+        self.assertEqual(tag.name, 'b')
+
+        # 改变tag的名字，对应的文档也会发生变化
+        tag.name = 'h1'
+        self.assertEqual(str(soup),
+                         '<html><head></head><body><h1 class="boldest">Extremely bold</h1></body></html>')
+
+    def test_tag_attribute(self):
+        soup = BeautifulSoup('<b class="boldest">Extremely bold</b>', "html5lib")
+        tag = soup.b
+
+        # 属性
+        self.assertEqual(tag['class'], ['boldest'])
+        self.assertEqual(tag.attrs, {'class': ['boldest']})
+
+        # 同样可以通过赋值的方式来修改属性
+        pass
